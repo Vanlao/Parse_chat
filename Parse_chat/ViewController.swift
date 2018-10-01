0e3d44b9cc061daf5caf453eee92fb536729e2c1
Tu Pham <tupham@tus-macbook-pro.local>
2018-09-30 19:49:46 -0700

    Initial Commit

---
+++ Parse_chat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Parse_chat
+//
+//  Created by Tu Pham on 9/30/18.
+//  Copyright © 2018 Van Lao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
